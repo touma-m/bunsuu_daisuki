94fe4750ce587d046e53754983cf9babeccfa054
touma_m <b215110@cc.it-hiroshima.ac.jp>
2018-01-12 18:39:34 +0900

    ハンドまわり

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardScript : MonoBehaviour, IPointerClickHandler
+{
+
+    bool CardTrigger;
+    GameObject Hand;
+    int Mynun;//何番目に選ばれているか
+
+
+    // Use this for initialization
+    void Start () {
+        CardTrigger = false;
+        Hand= GameObject.Find("Hand");
+    }
+	
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData data)
+    {
+        //CardTrigger = true;
+        Hand.GetComponent<HandScript>().SelectOrder(Mynun);
+        Debug.Log(Mynun);
+        Debug.Log("でしでし　怒った？");
+    }
+    public bool getTrigger() {
+        return CardTrigger;
+    }
+    public void setnun(int num) {
+        Mynun = num;
+    }
+}
