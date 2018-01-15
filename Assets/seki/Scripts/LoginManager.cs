eeaee4e49fb5a69cd9f3665bfb066e14cae66b0b
ryosuke-s <b215064@cc.it-hiroshima.ac.jp>
2018-01-15 16:52:15 +0900

    余計なものを消した

---
+++ Assets/seki/Scripts/LoginManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManager : MonoBehaviour {
+
+
+	[SerializeField]
+	Text joinedMembersText;
+	Room room;
+
+	void Start(){
+		room = PhotonNetwork.room;
+	}
+		
+
+	void Update(){
+		UpdateMemberList();
+	}
+
+	// <summary>
+	// リモートプレイヤーが入室した際にコールされる
+	// </summary>
+	public void OnPhotonPlayerConnected(PhotonPlayer player)
+	{
+		Debug.Log(player.name + " is joined.");
+	}
+
+	// <summary>
+	// リモートプレイヤーが退室した際にコールされる
+	// </summary>
+	public void OnPhotonPlayerDisconnected(PhotonPlayer player)
+	{
+		Debug.Log(player.name + " is left.");
+	}
+
+	public void UpdateMemberList()
+	{
+		joinedMembersText.text = "";
+		
+		foreach (var p in PhotonNetwork.playerList)
+		{
+			joinedMembersText.text += p.name + "\n";
+		}
+	}
+			
+}
